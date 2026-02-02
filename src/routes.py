11f53764ded1c35f1215d16c74263fbf434f1f7d
Alcides Rivera <elarsi626@gmail.com>
2026-02-02 17:48:44 +0000

    t

--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from models import User,db
 
 
@@ -12,8 +12,23 @@ def get_users():
 
 
 @api.route("/users/<int:user_id>", methods=["GET"])
-def get_user():
-    user = User.query.all(user_id)
+def get_user(user_id):
+    user = User.query.get(user_id)
     if not user:
         return jsonify({"error": "User not found"}), 400
     return jsonify(user.serialize()), 200
+
+@api.route("/users", methods=["POST"])
+def create_user():
+    data = request.get_json()
+    if not data.get("email") or not data.get("password"):
+        return jsonify({"error": "Missing fields"}), 400
+    
+    new_user = User(
+        email=data["email"],
+        password=data["password"]
+    )
+
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.serialize()), 201
